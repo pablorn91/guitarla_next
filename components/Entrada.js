0ff6ebc9b8e9086b3e2968eeb46c1de9716e031e
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-02-19 13:16:56 -0400

    Configurar strapi, Entrada.js, module.css y configuracion API

--- components/Entrada.js
+++ components/Entrada.js
@@ -4,7 +4,7 @@ import { formatearFecha } from '../helpers'
 import styles from '../styles/Entrada.module.css'
 
 const Entrada = ({entrada}) => {
-    const {  titulo, resumen, imagen, published_at, id } = entrada;
+    const {  titulo, resumen, imagen, published_at, id, url } = entrada;
     return (
         <article>
             <Image priority="true" layout='responsive' width={800} height={600} src={imagen.url} alt={`imagen blog ${titulo}`} />
@@ -12,7 +12,7 @@ const Entrada = ({entrada}) => {
                 <h3>{titulo}</h3>
                 <p className={styles.fecha}>{formatearFecha(published_at)}</p>
                 <p className={styles.resumen}>{resumen}</p>   
-                <Link href={`/blog/${id}`}>
+                <Link href={`/blog/${url}`}>
                 Leer entrada</Link>
             </div>
         </article>
