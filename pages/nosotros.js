6f7e169720e51a525036bf5428fb23d938227842
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-02-19 09:15:24 -0400

    nosotros.js y modulo css creado

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import Layout from '../components/Layout'
+import styles from '../styles/Nosotros.module.css'
 
 const Nosotros = () => {
   return (
@@ -6,7 +8,19 @@ const Nosotros = () => {
         <Layout
             pagina='Nosotros'
         >
-            <h1>Desde Nosotros</h1>
+            <main className='contenedor'>
+                <h2 className='heading'>Nosotros</h2>
+
+                <div className={styles.contenido}>
+
+                  <Image layout='responsive' src="/img/nosotros.jpg" width={600} height={450} alt="Imagen sobre nosotros"/>
+
+                  <div>
+                     <p>Integer odio nibh, maximus vel blandit a, ullamcorper sagittis nulla. Nullam augue urna, elementum vel mollis vitae, pulvinar vel tellus. Quisque sed ornare felis, non imperdiet lorem. Aliquam id volutpat nibh. Nullam aliquet lacus non elit efficitur, non pretium mi dapibus. Mauris id ultrices libero. Sed hendrerit, mauris in tincidunt tincidunt, sem tortor tempus purus, nec scelerisque turpis nisi id leo. Vivamus vehicula tellus ipsum, in consectetur mauris  commodo m feugiat. Vivamus pharetra eu justo sed feugiat. Vivamus ornare sapien nec nisi venenatis finibus. Pellentesque eget erat nulla. Nullam iaculis quam at enim commodo porttitor.</p>
+                     <p>Integer odio nibh, maximus vel blandit a, ullamcorper sagittis nulla. Nullam augue urna, elementum vel mollis vitae, pulvinar vel tellus. Quisque sed ornare felis, non imp id volutpat nibh. Nullam aliquet lacus non elit efficitur, non pretium mi dapibus. Mauris id ultrices libero. Sed hendrerit, mauris in tincidunt tincidunt, sem tortor tempus purus, nec scelerisque turpis nisi id leo. Vivamus vehicula tellus ipsum, in consectetur mauris  commodo mi quis turpis aliquam feugiat. Vivamus pharetra eu justo sed feugiat. Vivamus ornare sapien nec nisi venenatis finibus. Pellentesque eget erat nulla. Nullam iaculis quam at enim commodo porttitor.</p>
+                  </div>
+                </div>
+            </main>
       </Layout>
     </div>
   )
