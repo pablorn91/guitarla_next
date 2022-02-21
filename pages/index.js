6e45e7b9216a1e01b51adaa09567c617b65dbfb4
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-02-21 10:27:15 -0400

    index con guitarras y seccion curso

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,51 @@
 import Layout from '../components/Layout'
+import Listado from '../components/Listado'
+import Curso from '../components/Curso'
 
-export default function Home() {
+export default function Home({ guitarras, curso }) {
   return (
     <div>
       <Layout
         pagina='Inicio'
       >
-      <h1>Desde Inicio</h1>
+      <main className='contenedor'>
+        <h1 className='heading'>Nuestra Colección</h1>
+        <Listado
+          guitarras={guitarras}
+        />
+      </main>
+
+      <Curso 
+        curso={curso}
+      />
+
       </Layout>
 
     </div>
   )
+  
+}
+
+export async function getServerSideProps() {
+
+  const urlGuitarras = `${process.env.API_URL}/guitarras`
+  const urlCurso = `${process.env.API_URL}/cursos`
+
+  const [ resGuitarras, resCurso ] = await Promise.all([
+    fetch(urlGuitarras),
+    fetch(urlCurso)
+  ])
+
+  const [ guitarras, curso ] = await Promise.all([
+    resGuitarras.json(),
+    resCurso.json()
+  ])
+ 
+
+  return {
+    props: {
+      guitarras,
+      curso
+    }
+  }
 }
